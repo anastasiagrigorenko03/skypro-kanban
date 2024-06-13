976b47d549d00e8c955e805bad31bc78e256fc97
anastasiagrigorenko03 <nastena.grigorenko.03@mail.ru>
2024-06-13 23:40:47 +0300

    ДЗ-1

---
+++ src/components/Column/index.jsx
@@ -0,0 +1,13 @@
+import { Card } from "../Card";
+export const Column = ({title}) => {
+    return (
+        <div className="main__column column">
+							<div className="column__title">
+								<p>{title}</p>
+							</div>
+							<div className="cards">
+							<Card />
+							</div>
+						</div>	
+    )
+}
---
+++ src/components/Main/index.jsx
@@ -0,0 +1,20 @@
+import { Column } from "../Column";
+export const Main = () => {
+    return (
+        <main className="main">
+			<div className="container">
+				
+				<div className="main__block">
+					<div className="main__content">
+              <Column title="Без заголовка"/>
+			  <Column title="Нужно сделать"/>
+			  <Column title="В работе"/>
+			  <Column title="Тестирование"/>
+			  <Column title="Готово"/>
+					</div>
+				
+				</div>
+			</div>
+		</main>
+    )
+}
